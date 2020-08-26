e7a972164f0d0e87c58c867e14cb5db8f8368528
linhe <gavinjs.me@gmail.com>
2020-08-26 19:20:56 +0800

    自动生成配置和自动生成目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const {blogSideBar, bookSideBar, abstractSideBar} = require('./readdir')
+
 module.exports = {
   title: '林河前端之路',
   description: '前端笔记整理',
@@ -37,112 +39,15 @@ module.exports = {
     logo: '/linhe.jpg',
     nav:[
       { text: 'Home', link: '/'},
-      { text: '博文', link: '/pages/blog/javascript/Webpack配置指南' }, // 内部链接 以docs为根目录
-      { text: '摘要', link: '/pages/abstract/code/一行代码实现时间戳转时分秒'},
-      { text: '读书', link: '/pages/book/js-programme/01-Javascript简介' },
+      { text: '博文', link: '/pages/blog/' }, // 内部链接 以docs为根目录
+      { text: '摘要', link: '/pages/abstract/'},
+      { text: '读书', link: '/pages/book/' },
       { text: 'GitHub', link: 'https://github.com/imlinhe'}  
     ],
     sidebar: {
-      '/pages/blog/': [         
-        {
-          title: 'JavaScript',   // 一级菜单名称
-          collapsable: false, // false为默认展开菜单, 默认值true是折叠,
-          children: [
-            ['javascript/Webpack配置指南', 'Webpack配置指南'],
-            ['javascript/JavaScript基础', 'JavaScript基础'],
-            ['javascript/前端实现图片压缩上传', '前端实现图片压缩上传'],
-            ['javascript/数组去重方式', '数组去重方式'],
-            ['javascript/encodeURI和encodeURIComponent', 'encodeURI和encodeURIComponent'],
-            ['javascript/原型和原型链', '原型和原型链'],
-            ['javascript/防抖和节流', '防抖和节流'],
-            ['javascript/函数柯里化', '函数柯里化'],
-            ['javascript/字符串方法', '字符串方法'],
-            ['javascript/谈谈get和post的区别', '谈谈get和post的区别'],
-          ]
-        },
-        {
-          title: 'CSS',
-          collapsable: false,
-          children: [
-            ['css/Css基础', 'Css基础'],
-            ['css/Css3和Html5新特性不完全手册', 'Css3和Html5新特性不完全手册'],
-            ['css/Css3伪类', 'Css3伪类'],
-          ]
-        },
-        {
-          title: 'Vue', 
-          collapsable: false,
-          children: [
-            ['vue/Vue项目性能优化', 'Vue项目性能优化'],
-            ['vue/Vue_history模式配置', 'Vue_history模式配置'],
-            ['vue/Vue实战项目总结', 'Vue实战项目总结'],
-            ['vue/Element远程搜索Bug', 'Element远程搜索Bug'],
-            ['vue/Vue面试题汇总', 'Vue面试题汇总'],
-          ]
-        },
-        {
-          title: 'Git',   
-          collapsable: false, 
-          children: [
-            ['git/Git常用命令', 'Git常用命令'], 
-            ['git/Git配置和错误', 'Git配置和错误'], 
-            ['git/Git生成多个公钥', 'Git生成多个公钥'], 
-          ]
-        },
-        {
-          title: 'Others',
-          collapsable: false,
-          children: [
-            ['others/跨域解决方案', '跨域解决方案'],
-            ['others/一道算法题', '一道算法题'],
-            ['others/VSCode插件和配置', 'VSCode插件和配置'],
-            ['others/win软件安装备忘', 'win软件安装备忘'],
-            ['others/彻底理解浏览器的缓存机制', '彻底理解浏览器的缓存机制'],
-            ['others/在第三方页面调用微信接口', '在第三方页面调用微信接口'],
-            ['others/移动端遮罩阻止滚动方案', '移动端遮罩阻止滚动方案'],
-            ['others/Content-type的几种常见类型', 'Content-type的几种常见类型'],
-            ['others/前端安全', '前端安全'],
-          ]
-        }
-      ],
-      '/pages/book/': [
-        {
-          title: 'JS高级程序设计',   // 一级菜单名称
-          collapsable: false, // false为默认展开菜单, 默认值true是折叠,
-          children: [
-            ['js-programme/01-JavaScript简介', '01-JavaScript简介'],
-            ['js-programme/02-在HTML中使用JavaScript', '02-在HTML中使用JavaScript']
-          ]
-        },
-      ],
-      '/pages/abstract/': [
-        { 
-          title: '代码摘要', 
-          collapsable: false,
-          children: [
-            ['code/一行代码实现时间戳转时分秒', '一行代码实现时间戳转时分秒'],
-            ['code/饿了么Dialog组件二次封装', '饿了么Dialog组件二次封装'],
-            ['code/椭圆运动轨迹', '椭圆运动轨迹'],
-            ['code/常用公共样式', '常用公共样式'],
-            ['code/常用scss函数表', '常用scss函数表'],
-            ['code/公共方法汇总', '公共方法汇总'],
-            ['code/浮点数运算误差', '浮点数运算误差'],
-            ['code/isEqual', 'isEqual'],
-            ['code/ESLint配置项', 'ESLint配置项'],
-            ['code/电话号码分段', '电话号码分段'],
-            ['code/Canvas签名', 'Canvas签名']
-          ]
-        },
-        {
-          title: '其他',
-          collapsable: false,
-          children: [
-            ['others/切换镜像源', '切换镜像源'],
-            ['others/Cmder右键启动', 'Cmder右键启动'],
-            ['others/Flat-UI色表', 'Flat-UI色表'],
-          ]
-        }
-      ]
+      '/pages/blog/': blogSideBar,
+      '/pages/book/': bookSideBar,
+      '/pages/abstract/': abstractSideBar
       //...可添加多个不同的侧边栏，不同页面会根据路径显示不同的侧边栏
     }
   }
